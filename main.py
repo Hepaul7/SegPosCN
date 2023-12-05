2cb96c934370cb3f4cf1dc5af997863d7788a65a
Hepaul7 <paulhe0773@gmail.com>
2023-12-05 20:20:07 +0100

    up to date versions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from embeddings import *
-from encodings import *
-
-data = read_csv('data/CTB7/dev.tsv')
-texts = extract_sentences(data)
-bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-input_ids, attention_masks = prepare(texts, bert_tokenizer)
-
-model = load_model('bert-base-chinese')
-embeddings = get_bert_embeddings(model, input_ids, attention_masks)
-
-encoder = Encoder()
-encoder_output = encoder()
+from embedding_layer import *
+from encoding_layer import *
+from decoding_layer import *
+#
+#
+print('running CTB7...\n')
+# # data = read_csv('data/CTB7/dev.tsv')
+# # texts = extract_sentences(data)
+# # bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+# # input_ids, attention_masks = prepare(texts, bert_tokenizer)
+# #
+# # model = load_model('bert-base-chinese')
+# # print('embedding CTB7...\n')
+# # embeddings = get_bert_embeddings(model, input_ids, attention_masks)
+# #
+# # encoder = Encoder()
+# # encoder_output = encoder()
+#
+#
+# # Test Run
+#
+# # sample sentence
---
+++ training.py
@@ -0,0 +1,15 @@
+# import torch
+# import torch.nn as nn
+# from TorchCRF import CRF
+#
+#
+# def train(model: torch.nn, train_data: torch.tensor, optimizer, criterion):
+#     """
+#     Training Loop
+#     :param model:
+#     :param train_data:
+#     :param optimizer:
+#     :param criterion:
+#     :return:
+#     """
+#     raise NotImplementedError
