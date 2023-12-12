180f1656c5bde383297e3e9aff0f5800e054cd76
Hepaul7 <paulhe0773@gmail.com>
2023-12-12 15:12:08 +0100

    removed bert based transformer for encoding

--- transformer.py
+++ transformer.py
@@ -149,7 +149,27 @@ class EncoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
-    "Core encoder is a stack of N layers"
+    """
+    An encoder with self-attention mechanism.
+
+    Why?
+    Sequence modelling (RNN and CNNs), suffer from long term dependency problems.
+    It cannot extract non-local interactions in a sentence (Qiu et al. 2020). Hence, we adapt the
+    Transformer encoder (Vaswani et al. 2017)
+
+    The following are based on (Vaswani et al. 2017) section 3.2.1
+    Scaled Dot Product Attention
+    We are given sequence of vectors H \in \mathbb{R}^{(T+1) \times d_{model}}. A single
+    head, self-attention projects H into:
+        - Q: The Query Matrix \in \mathbb{R}^{(T+1) \times d_{k}}
+        - K: The Key Matrix \in \mathbb{R}^{(T+1) \times d_{k}}
+        - V: The Value Matrix \mathbb{R}^{(T+1) \times d_{v}}
+
+    Attention(Q, K, V) = softmax(\frac{QK^T}{\sqrt{d_k}})V
+
+    Adapting a similar structure as (Qiu et al. 2020). The encoder consists of
+    six multi-head self-attention layers and fully connected layers.
+    """
 
     def __init__(self, layer, N):
         super(Encoder, self).__init__()
