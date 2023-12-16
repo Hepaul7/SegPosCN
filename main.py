79476a119142167f31618ce48600e6ee1819d983
Hepaul7 <paulhe0773@gmail.com>
2023-12-16 18:41:45 +0100

    need proper output embeddings

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
-from transformer.embedding_layer import *
+from transformers import AdamW
+
+from transformer.input_embeddings import *
 from transformer.encoder import make_encoder
 from transformer.decoder import make_decoder
+from models import CWSPOSTransformer
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+
+from torch.utils.data import DataLoader, TensorDataset
+
+#####
+# This file is for testing (for now)
+#####
 print('running CTB7...\n')
-data = read_csv('data/CTB7/dev.tsv')
+data = read_csv('data/CTB7/train.tsv')
 
-texts, max_len = extract_sentences(data)
-# labels, max_len_2 = extract_labels(data)
+texts, tags, max_len = extract_sentences(data)
 
-# assert max_len == max_len_2
-
-# prep_text = prepare_data(texts, max_len)
-# prep_label = prepare_data(labels, max_len)
 bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-# data_tensor = convert_to_tensor(texts, max_len)
-input_ids, attention_masks = prepare(texts, bert_tokenizer, max_len)
 
-
+input_ids, attention_masks = prepare(texts, bert_tokenizer, 64)  # includes BOS/EOS
+output_ids, output_masks = prepare_outputs(tags, 64)  # includes BOS/EOS
 model = load_model('bert-base-chinese')
-# device = torch.device('mps')
-# model.to(device)
-# get_bert_embeddings(model, input_ids, attention_masks)
-input_embeddings = get_bert_embeddings(model, input_ids, attention_masks)
 
-# input_ids, attention_masks = prepare(labels, bert_tokenizer)
-# #
-# # encoder = Encoder()
-# # encoder_output = encoder()
-#
-#
+# input_embeddings = get_bert_embeddings(model, input_ids, attention_masks)
+# output_embeddings = get_bert_embeddings(model, decoder_input_ids, decoder_attention_mask)
+encoder = make_encoder()
+decoder = make_decoder()
+segpos_model = CWSPOSTransformer(encoder, decoder, output_size=33 * 4 + 2, d_model=768)
 
-# Test Run to make sure bug free
-# sample sentence
+# change to embeddings later
+dataset = TensorDataset(input_ids, attention_masks, output_ids, output_masks)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
 
+num_epochs = 5
+optimizer = AdamW(segpos_model.parameters(), lr=5e-5)
+loss_function = CrossEntropyLoss()
+
+for epoch in range(num_epochs):
+    segpos_model.train()
+    total_loss = 0
+    for batch in dataloader:
+        batch_input_ids, batch_attention_masks, batch_output_ids, batch_output_masks = batch
+
+        input_embeddings = get_bert_embeddings(model, batch_input_ids, batch_attention_masks)
+
+        print(input_embeddings.shape)
+        predictions = segpos_model(input_embeddings, batch_attention_masks, batch_output_ids, batch_output_masks)
+
+        loss = loss_function(predictions.view(-1, 33 * 4 + 2), batch_output_ids.view(-1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
 
---
+++ transformer/output_embeddings.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import math
+
+
+# draft
+class OutputEmbedder(torch.nn.Module):
+    def __init__(self, num_labels, embedding_dim):
+        super(OutputEmbedder, self).__init__()
+        self.embedding = torch.nn.Embedding(num_labels, embedding_dim)
+
+    def forward(self, label_ids):
+        return self.embedding(label_ids)
+
+
+class PositionalEncoder(nn.Module):
+    """
+    Since transformer models contain no recurrence or convolution, we need
+    to inject some information about relative or absolute position of the
+    sequence (Vaswani et al. 2017)
+
+    Add positional encodings to the input embeddings before the encoder
+    layer.
+
+    Implementation of this class is based on section 3.5 of Attention is
+    all you need (Vaswani et al. 2017)
+    """
+    def __init__(self, d_model: int, drop_out: float, max_len: int = 5000):
+        """
+        :param d_model: dimension of the model, if BERT it would be 768
+        :param drop_out: drop out rate
+        :param max_len: max length
+        """
+        super(PositionalEncoder, self).__init__()
+        self.dropout = nn.Dropout(p=drop_out)
+
+        pe = torch.zeros(max_len, d_model).float()
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply sine function to even indices (pos, 2i)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply cosine function to odd indices (pos, 2i + 1)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        # For batch processing
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.tensor):
+        x += self.pe[:, x.size(0), :]
+        return self.dropout(x)
+
