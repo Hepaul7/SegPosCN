f118fbd1a2238051b7444f28fae1f07196deb678
Hepaul7 <paulhe0773@gmail.com>
2023-12-16 13:23:38 +0100

    turns out, original version worked too. max sentence is too long, 64 is acceptable.

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from transformer.decoder import make_decoder
 
 print('running CTB7...\n')
 data = read_csv('data/CTB7/dev.tsv')
-texts, max_len = extract_sentences(data)
-labels, max_len_2 = extract_labels(data)
 
-assert max_len == max_len_2
+texts, max_len = extract_sentences(data)
+# labels, max_len_2 = extract_labels(data)
+
+# assert max_len == max_len_2
 
 # prep_text = prepare_data(texts, max_len)
 # prep_label = prepare_data(labels, max_len)
-
 bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-
+# data_tensor = convert_to_tensor(texts, max_len)
 input_ids, attention_masks = prepare(texts, bert_tokenizer, max_len)
-# input_ids, attention_masks = prepare(texts, bert_tokenizer)
+
 
 model = load_model('bert-base-chinese')
-# input_embeddings = get_bert_embeddings(model, input_ids, attention_masks)
+# device = torch.device('mps')
+# model.to(device)
+# get_bert_embeddings(model, input_ids, attention_masks)
+input_embeddings = get_bert_embeddings(model, input_ids, attention_masks)
 
 # input_ids, attention_masks = prepare(labels, bert_tokenizer)
 # #
--- transformer/embedding_layer.py
+++ transformer/embedding_layer.py
@@ -30,6 +30,78 @@ def read_csv(path: str) -> List[List[str]]:
     return data
 
 
+def extract_sentences(data: List[List[str]]) -> (List[str], int):
+    """ Breaks down some text into individual sentence
+    :param data: List of lists containing character and a label
+    :return: List of strings, where each string is a Chinese sentence
+    """
+    sentences = []
+    max_len = 0
+    curr = ''
+    total_len = 0
+    for ls in data:
+        if len(ls) > 0:
+            curr += ls[0]
+            continue
+        if curr != '':
+            if len(curr) > max_len:
+                max_len = len(curr)
+                print(max_len)
+            sentences.append(curr)
+            total_len += len(curr)
+            curr = ''
+    print(f'avg_len: {total_len // len(sentences)}')
+    return sentences, max_len
+
+
+def extract_characters_from_text(text: str) -> List[str]:
+    """ Break down sentences into individual characters from text
+    :param text: string of texts
+    :return: List of characters
+    """
+    return [char for char in text]
+
+
+def prepare(texts: List[str], tokenizer: BertTokenizer, max_len: int) -> [torch.Tensor, torch.Tensor]:
+    """ Prepare texts into a form that can be fed into a
+    pretrained transformer (Chinese-BERT)
+    :param texts: List of strings, where each string is a Chinese sentence
+    :param tokenizer: Tokenizer
+    :return: input_ids: tokenized representation of text,
+             attention_masks: tensors of 1s and 0s
+    """
+    input_ids = []
+    attention_masks = []
+    for text in texts:
+        char_tokens = extract_characters_from_text(text)
+        encoded_dict = tokenizer.encode_plus(
+            char_tokens,
+            add_special_tokens=True,
+            max_length=128,
+            truncation=True,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+            is_split_into_words=False
+        )
+        input_ids.append(encoded_dict['input_ids'])
+        attention_masks.append(encoded_dict['attention_mask'])
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    return input_ids, attention_masks
+
+
+def load_model(model_name):
+    model = BertModel.from_pretrained(model_name)
+    return model
+
+
+def get_bert_embeddings(model, input_ids, attention_masks):
+    with torch.no_grad():
+        outputs = model(input_ids, attention_mask=attention_masks)
+        last_hidden_states = outputs.last_hidden_state
+    return last_hidden_states
+
 # def extract_sentences(data: List[List[str]]) -> (List[str], int):
 #     """ Breaks down some text into individual sentence
 #     :param data: List of lists containing character and a label
@@ -50,101 +122,144 @@ def read_csv(path: str) -> List[List[str]]:
 #
 #     return sentences, max_len
 
-def extract_sentences(data: List[List[str]]) -> (List[List[str]], int):
-    """ Breaks down some text into individual sentence
-    :param data: List of lists containing character and a label
-    :return: List of strings, where each string is a Chinese sentence
-    """
-    sentences = []
-    curr = []
-    max_len = 0
-    for ls in data:
-        if len(ls) > 0:
-            curr.append(ls[0])
-            continue
-        if curr:
-            sentences.append(curr)
-            if len(curr) > max_len:
-                max_len = len(curr)
-            curr = []
+# def extract_sentences(data: List[List[str]]) -> (List[List[str]], int):
+#     """ Breaks down some text into individual sentence
+#     :param data: List of lists containing character and a label
+#     :return: List of strings, where each string is a Chinese sentence
+#     """
+#     sentences = []
+#     curr = []
+#     max_len = 0
+#     for ls in data:
+#         if len(ls) > 0:
+#             curr.append(ls[0])
+#             continue
+#         if curr:
+#             sentences.append(curr)
+#             if len(curr) > max_len:
+#                 max_len = len(curr)
+#             curr = []
+#
+#     return sentences, max_len
 
-    return sentences, max_len
-
-
-def extract_labels(data: List[List[str]]) -> (List[List[str]], int):
-    """ Breaks down some text into their tags
-    :param data:
-    :return: List of tags
-    """
-    tags = []
-    curr = []
-    max_len = 0
-    for ls in data:
-        if len(ls) > 0:
-            curr.append(ls[1])
-            continue
-        if curr:
-            tags.append(curr)
-            if len(curr) > max_len:
-                max_len = len(curr)
-            curr = []
-    return tags, max_len
-
-
-def prepare_data(data: List[List[str]], max_len: int) -> List[List[str]]:
-    """
-    Add padding, BOS, EOS
-    :param data:
-    :param max_len:
-    :return:
-    """
-    for sentence in data:
-        sentence.insert(0, BOS_WORD)
-        sentence.append(EOS_WORD)
-        while len(sentence) < max_len:
-            sentence.append(PAD_WORD)
-    return data
-
-
-def prepare(data: List[List[str]], tokenizer: BertTokenizer, max_length: int) -> [torch.Tensor, torch.Tensor]:
-    """ Prepare texts into a form that can be fed into a
-    pretrained transformer (Chinese-BERT)
-    :param data: List of strings, where each string is a Chinese sentence
-    :param tokenizer: Tokenizer
-    :param max_length: max length of sentence within batch
-    :return: input_ids: tokenized representation of text,
-             attention_masks: tensors of 1s and 0s
-    """
-    joined_sentences = [''.join(sentence) for sentence in data]
-
-    encoding = tokenizer(
-        joined_sentences,
-        add_special_tokens=True,
-        max_length=max_length,
-        truncation=True,
-        padding='max_length',
-        return_attention_mask=True,
-        return_tensors='pt',
-    )
-
-    # Extract input_ids and attention_masks
-    input_ids = encoding['input_ids']
-    attention_masks = encoding['attention_mask']
-
-    return input_ids, attention_masks
-
-
-def load_model(model_name):
-    model = BertModel.from_pretrained(model_name)
-    return model
-
-
-def get_bert_embeddings(model, input_ids, attention_masks):
-    with torch.no_grad():
-        outputs = model(input_ids, attention_mask=attention_masks)
-        last_hidden_states = outputs.last_hidden_state
-    return last_hidden_states
-
+# def extract_sentences(data: List[List[str]]) -> (List[List[str]], int):
+#     """ Breaks down some text into individual sentence
+#     :param data: List of lists containing character and a label
+#     :return: List of strings, where each string is a Chinese sentence
+#     """
+#     sentences = []
+#     curr = []
+#     max_len = 0
+#     for ls in data:
+#         if len(ls) > 0:
+#             curr.append(ls[0])
+#             continue
+#         if curr:
+#             sentences.append(curr)
+#             if len(curr) > max_len:
+#                 max_len = len(curr)
+#             curr = []
+#
+#     return sentences, max_len
+#
+#
+# def convert_to_tensor(data: List[List[str]], max_len: int) -> torch.tensor:
+#     """
+#
+#     :param max_len:
+#     :param data:
+#     :return:
+#     """
+#
+#     data_tensor = torch.zeros(len(data), max_len, dtype=torch.int)
+#     sentence_index = 0
+#     for i, sentence in enumerate(data):
+#         if sentence:  # Check if not empty
+#             for j, char in enumerate(sentence):
+#                 data_tensor[i, j] = ord(char)
+#
+#     return data_tensor
+#
+#
+# def extract_labels(data: List[List[str]]) -> (List[List[str]], int):
+#     """ Breaks down some text into their tags
+#     :param data:
+#     :return: List of tags
+#     """
+#     tags = []
+#     curr = []
+#     max_len = 0
+#     for ls in data:
+#         if len(ls) > 0:
+#             curr.append(ls[1])
+#             continue
+#         if curr:
+#             tags.append(curr)
+#             if len(curr) > max_len:
+#                 max_len = len(curr)
+#             curr = []
+#     return tags, max_len
+#
+#
+# def prepare_data(data: List[List[str]], max_len: int) -> List[List[str]]:
+#     """
+#     Add padding, BOS, EOS
+#     :param data:
+#     :param max_len:
+#     :return:
+#     """
+#     for sentence in data:
+#         sentence.insert(0, BOS_WORD)
+#         sentence.append(EOS_WORD)
+#         while len(sentence) < max_len:
+#             sentence.append(PAD_WORD)
+#     return data
+#
+#
+# def prepare(data: List[List[str]], tokenizer: BertTokenizer, max_length: int) -> [torch.Tensor, torch.Tensor]:
+#     """ Prepare texts into a form that can be fed into a
+#     pretrained transformer (Chinese-BERT)
+#     :param data: List of strings, where each string is a Chinese sentence
+#     :param tokenizer: Tokenizer
+#     :param max_length: max length of sentence within batch
+#     :return: input_ids: tokenized representation of text,
+#              attention_masks: tensors of 1s and 0s
+#     """
+#     joined_sentences = [''.join(sentence) for sentence in data]
+#
+#     encoding = tokenizer(
+#         joined_sentences,
+#         add_special_tokens=True,
+#         max_length=max_length,
+#         truncation=True,
+#         padding='max_length',
+#         return_attention_mask=True,
+#         return_tensors='pt',
+#     )
+#
+#     # Extract input_ids and attention_masks
+#     input_ids = encoding['input_ids']
+#     attention_masks = encoding['attention_mask']
+#
+#     return input_ids, attention_masks
+#
+#
+# def load_model(model_name):
+#     model = BertModel.from_pretrained(model_name)
+#     return model
+#
+#
+# def get_bert_embeddings(model, input_ids, attention_masks):
+#     model.eval()
+#
+#     input_ids = input_ids.to(model.device)
+#     attention_masks = attention_masks.to(model.device)
+#
+#     with torch.no_grad():
+#         outputs = model(input_ids=input_ids, attention_mask=attention_masks)
+#         embeddings = outputs.last_hidden_state
+#
+#     return embeddings
 
 # Note to self, perhaps adapt MCCWS 3 embeddings?
 # class PositionalEncoder(nn.Module):
@@ -200,4 +315,3 @@ def get_bert_embeddings(model, input_ids, attention_masks):
 #     p_e = PositionalEncoder(d_model=768, drop_out=0.1)
 #     embeddings = p_e(embeddings)
 #     return embeddings
-
